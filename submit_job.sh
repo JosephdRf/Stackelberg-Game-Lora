e437662f8a91fc4ded20e2c4b92732338ccd47d2
Joseph de Roffignac <joseph.deroffignac@gmail.com>
2026-04-30 00:29:56 -0400

    29/04

--- submit_job.sh
+++ submit_job.sh
@@ -3,7 +3,7 @@
 #SBATCH --account=def-omar12
 #SBATCH --cpus-per-task=10
 #SBATCH --mem=16G
-#SBATCH --time=2:00:00
+#SBATCH --time=3:00:00
 #SBATCH --gres=gpu:a100:1
 #SBATCH --output=logs/%j.out
 #SBATCH --error=logs/%j.err
