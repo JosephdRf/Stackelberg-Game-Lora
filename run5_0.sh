67be916eaaab44f0619e977c661fea517c1035fa
Joseph de Roffignac <joseph.deroffignac@gmail.com>
2026-05-22 16:03:07 -0400

    22/05

---
+++ run5_0.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+#SBATCH --account=def-omar12
+#SBATCH --cpus-per-task=10
+#SBATCH --mem=16G
+#SBATCH --time=15:00:00
+#SBATCH --gres=gpu:a100:1
+#SBATCH --output=logs/%j.out
+#SBATCH --error=logs/%j.err
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH --mail-user=joseph.deroffignac@gmail.com
+
+RUN_NAME=Exp5_0
+scontrol update JobId=$SLURM_JOB_ID JobName=$RUN_NAME
+
+# Modules
+module load StdEnv/2023
+module load python/3.10
+module load cuda/12.2
+module load intel/2023.2.1
+module load arrow/21.0.0
+
+# Aller au projet
+cd "$SLURM_SUBMIT_DIR"
+
+# Purge des anciens logs (garder les 10 derniers)
+ls -t logs/*.out 2>/dev/null | tail -n +11 | xargs -r rm --
+ls -t logs/*.err 2>/dev/null | tail -n +11 | xargs -r rm --
+
+# Virtualenv
+source $SLURM_SUBMIT_DIR/.venv/bin/activate
+
+# Offline mode
+export WANDB_MODE=offline
+export HF_DATASETS_OFFLINE=1
+export TRANSFORMERS_OFFLINE=1
+
+CKPT_DIR=$SLURM_SUBMIT_DIR/checkpoints/exp5/$RUN_NAME
+
+python pythia160M/baseline/train_baseline.py \
+    --output_dir $CKPT_DIR \
+    --wandb_project Stackelberg-Pythia160M --wandb_group Exp5 --run_name $RUN_NAME \
+    --no-lora \
+    --nb_runs 5 \
